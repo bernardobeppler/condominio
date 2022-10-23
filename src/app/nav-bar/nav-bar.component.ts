814594c8bb50fbf827d7105ce8e62465f227c0ca
Bernardo Beppler <betobeppler@gmail.com>
2022-10-23 11:35:47 -0300

    Adicionado dois componentes: nav-bar e footer

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,18 @@
+import * as M from 'materialize-css';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements AfterViewInit {
+  @ViewChild('mobile') sideNav?: ElementRef;
+
+  title = 'condominio';
+
+  ngAfterViewInit(): void {
+    M.Sidenav.init(this.sideNav?.nativeElement);
+}
+
+}
