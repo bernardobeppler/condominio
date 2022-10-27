442ee2627f4fb060fad2680bdb45a7a4003b4a32
Bernardo Beppler <betobeppler@gmail.com>
2022-10-27 19:50:17 -0300

    Utilizando Materialize
    Criado dois componentes, cabeçalho e rodapé
    Utilizado dois tipos de data-binding, Interpolation e Property Binding

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -8,7 +8,7 @@ import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angula
 })
 export class NavBarComponent implements AfterViewInit {
   @ViewChild('mobile') sideNav?: ElementRef;
-
+  isHideMenu = true;
   title = 'condominio';
 
   ngAfterViewInit(): void {
